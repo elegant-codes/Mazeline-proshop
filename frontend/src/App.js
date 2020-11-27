a3eb35b12445ac0cf1922cbe3d070072658db9fa
elegant-codes <ajakayetolu@gmail.com>
2020-11-27 18:42:23 +0100

    centered Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header/>
-      <main> 
+      <main className="py-3"> 
         <Container>
           <h1>Welcome to mazeline store</h1>
         </Container>
