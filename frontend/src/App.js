360dabfcdbe657a24c1c5f84b0fb4be4fd16fd7d
elegant-codes <ajakayetolu@gmail.com>
2020-11-27 17:40:56 +0100

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-
-function App() { 
+const App = () => { 
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header/>
+      <main> 
+        <Container>
+          <h1>Welcome to mazeline store</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   );
 }
 
