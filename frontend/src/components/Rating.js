0d9e0ef5bc008a65eb8fd7bccc486c54b31b878a
elegant-codes <ajakayetolu@gmail.com>
2020-11-29 20:56:39 +0100

    added react router

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,38 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Rating = ({text, value}) => {
+
+const Rating = ({text, value, color}) => {
   return (
     <div className='rating'>
       <span>
-        <i className={
+        <i style={{color}}
+        className={
           value >= 1 ? 'fas fa-star'
           : value >= 0.5 ? 'fas fa-star-half-alt'
           : 'far fa-star'}></i>
       </span>
 
       <span>
-        <i className={
+        <i style={{color}} 
+        className={
           value >= 2 ? 'fas fa-star'
           : value >= 2.5 ? 'fas fa-star-half-alt'
           : 'far fa-star'}></i>
       </span>
 
       <span>
-        <i className={
+        <i style={{color}} 
+        className={
           value >= 3 ? 'fas fa-star'
           : value >= 3.5 ? 'fas fa-star-half-alt'
           : 'far fa-star'}></i>
       </span>
 
       <span>
-        <i className={
+        <i style={{color}} 
+        className={
           value >= 4 ? 'fas fa-star'
           : value >= 4.5 ? 'fas fa-star-half-alt'
           : 'far fa-star'}></i>
       </span>
 
       <span>
-        <i className={
+        <i style={{color}} 
+        className={
           value >= 5 ? 'fas fa-star'
           : value >= 5.5 ? 'fas fa-star-half-alt'
           : 'far fa-star'}></i>
@@ -42,6 +49,16 @@ const Rating = ({text, value}) => {
 
     </div>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
 }
 
 export default Rating
