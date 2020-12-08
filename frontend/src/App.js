8f40548100fa179ff69e531118b1febf318362a8
elegant-codes <ajakayetolu@gmail.com>
2020-12-08 03:51:03 +0100

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowaerRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
