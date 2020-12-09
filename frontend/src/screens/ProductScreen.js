841fbce657264aea20a7d8e183b0a24d4a6cc12f
elegant-codes <ajakayetolu@gmail.com>
2020-12-09 19:47:15 +0100

    added add to cart

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -39,6 +39,33 @@ const ProductScreen = ({ match}) => {
 
             </ListGroup>
           </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                     {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Button className='btn-block' type='button' disabled={product.countInStock === 0 }>
+                    Add to cart
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
       </Row>
     </>
   )
